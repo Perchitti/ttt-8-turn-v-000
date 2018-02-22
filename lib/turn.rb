2aa5f016a688297ae2cfacda0aea0aa7d4df21f6
Lauren Perchitti <laurenperchitti@gmail.com>
2018-02-22 22:08:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,14 +20,15 @@ def move(board, index, player = "X")
 end
 
 
-
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+
 def position_taken? (board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
+
 
 def turn(board)
   puts "Please enter 1-9:"
