62b76e11503f34346924e255b9f22ffa2af40ea8
Lauren Perchitti <laurenperchitti@gmail.com>
2018-02-22 22:09:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_i - 1
-end
-
 
 def move(board, index, player = "X")
     board[index] = player
